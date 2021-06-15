90c9957314e4c1d10d7bdb945ab736fa8312c494
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-06-15 11:13:35 -0400

    Update outputs.tf
    Update outputs for demo usage

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "rg_name" {
   value = azurerm_resource_group.myresourcegroup.name
 }
+
+output "rg_name" {
+  value = azurerm_resource_group.myresourcegroup.name
+}
+
+output "tfver" {
+  value = var.tfver
+}
+
+output "prefix" {
+  value = var.prefix
+}
