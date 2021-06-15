bb0707f8a034acd8fc3471f931430b06b68cc674
kmruddy <kmruddy@gmail.com>
2021-06-15 14:11:17 -0400

    Config Consistency Update

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-  backend "remote" {
-    organization = "TPMM-Org"
-
-    workspaces {
-      name = "hashiapp-azure-resourcegroup"
-    }
-  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
