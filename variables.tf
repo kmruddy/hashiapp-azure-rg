cdcf74553e47486560b00f878c7140c4c0837dd6
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-06-15 11:42:41 -0400

    Update variables.tf
    variable cleanup

--- variables.tf
+++ variables.tf
@@ -19,17 +19,6 @@ variable "prefix" {
   default     = "ruddy"
 }
 
-variable "location" {
-  description = "The region where the virtual network is created."
-  default     = "centralus"
-}
-
-variable "address_space" {
-  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_prefix" {
-  description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
+variable "tfver" {
+  description = "Terraform version in use."
 }
